1aec39e5214fe3cdfd1672122fac4b1cbdbf1e5f
Ulrich <tonmoyulrich@gmail.com>
2023-05-30 16:53:36 +0600

    feat: logger msg color & clear and toggle logger msg type

--- src/Phoenix.Editor/Utilities/LoggerView.xaml.cs
+++ src/Phoenix.Editor/Utilities/LoggerView.xaml.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace Phoenix.Editor.Utilities
 {
-    /// <summary>
-    /// Interaction logic for LoggerView.xaml
-    /// </summary>
     public partial class LoggerView : UserControl
     {
         public LoggerView()
         {
             InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                Logger.Log(MessageType.Info, "Info Message");
+                Logger.Log(MessageType.Warning, "Warning Message");
+                Logger.Log(MessageType.Error, "Error Message");
+            };
+        }
+
+        private void OnClear_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Logger.Clear();
+        }
+
+        private void OnMsgFilter_Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var filter = 0x0;
+            if (toggleInfo.IsChecked == true) filter |= (int)MessageType.Info;
+            if (toggleWarn.IsChecked == true) filter |= (int)MessageType.Warning;
+            if (toggleError.IsChecked == true) filter |= (int)MessageType.Error;
+            Logger.SetMessageFilter(filter);
         }
     }
 }
