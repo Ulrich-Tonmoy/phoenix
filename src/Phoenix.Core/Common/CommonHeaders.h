67c4528fc5242a6d336aa8717faffe561517a5b5
Ulrich <tonmoyulrich@gmail.com>
2023-05-31 20:54:49 +0600

    feat: custom type name, entity and transform added to the core library

---
+++ src/Phoenix.Core/Common/CommonHeaders.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// C/C++
+#include <stdio.h>
+#include <assert.h>
+#include <typeinfo>
+
+// Custom
+#include "PrimitiveTypes.h"
---
+++ src/Phoenix.Core/Components/ComponentsCommon.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "CommonHeaders.h"
+#include "Id.h"
+
+namespace phoenix::game_entity {
+	DEFINE_TYPE_ID(entity_id);
+}
