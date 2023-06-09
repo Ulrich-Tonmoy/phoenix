3073d95ccaf097f4e59c03647b7434fe7f5a16ce
Ulrich <tonmoyulrich@gmail.com>
2023-06-09 19:01:49 +0600

    style: added custom control templates

--- src/Phoenix.Editor/Dictionaries/ControlTemplates.xaml.cs
+++ src/Phoenix.Editor/Dictionaries/ControlTemplates.xaml.cs
@@ -27,5 +27,23 @@ namespace Phoenix.Editor.Dictionaries
                 Keyboard.ClearFocus();
             }
         }
+
+        private void OnClose_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+        private void OnMaximizeRestore_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void OnMinimize_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+        }
     }
 }
