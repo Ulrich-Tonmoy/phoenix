19735a137de9ed9067578f1c043212b7e7b6b32e
Ulrich <tonmoyulrich@gmail.com>
2023-06-12 20:24:34 +0600

    chore: added ScalarBox control

---
+++ src/Phoenix.Editor/Utilities/Controls/ScalarBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Phoenix.Editor.Utilities.Controls
+{
+    class ScalarBox : NumberBox
+    {
+        static ScalarBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ScalarBox), new FrameworkPropertyMetadata(typeof(ScalarBox)));
+        }
+    }
+}
