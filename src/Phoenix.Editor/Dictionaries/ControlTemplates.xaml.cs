235efacd159dfe39001635871cf4ace85864bdc2
Ulrich <tonmoyulrich@gmail.com>
2023-06-12 18:42:30 +0600

    chore: added NumberBox control

--- src/Phoenix.Editor/Dictionaries/ControlTemplates.xaml.cs
+++ src/Phoenix.Editor/Dictionaries/ControlTemplates.xaml.cs
@@ -28,6 +28,39 @@ namespace Phoenix.Editor.Dictionaries
             }
         }
 
+        private void OnTextBoxEdit_KeyDown(object sender, KeyEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp == null) return;
+            if (e.Key == Key.Enter)
+            {
+                if (textBox.Tag is ICommand command && command.CanExecute(textBox.Text))
+                    command.Execute(textBox.Text);
+                else
+                    exp.UpdateSource();
+
+                textBox.Visibility = Visibility.Collapsed;
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                exp.UpdateTarget();
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnTextBoxEdit_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var exp = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (exp != null)
+            {
+                exp.UpdateTarget();
+                textBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Previous));
+            }
+        }
+
         private void OnClose_Button_Click(object sender, RoutedEventArgs e)
         {
             var window = (Window)((FrameworkElement)sender).TemplatedParent;
---
+++ src/Phoenix.Editor/Utilities/Controls/NumberBox.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Phoenix.Editor.Utilities.Controls
+{
+    [TemplatePart(Name = "PART_textBlock", Type = typeof(TextBlock))]
+    [TemplatePart(Name = "PART_textBox", Type = typeof(TextBox))]
+    class NumberBox : Control
+    {
+        private double _orginalValue;
+        private double _mouseXStart;
+        private double _multiplier;
+        private bool _captured = false;
+        private bool _valuChanged = false;
+
+        public double Multiplier
+        {
+            get => (double)GetValue(MultiplierProperty);
+            set => SetValue(MultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty MultiplierProperty = DependencyProperty.Register(nameof(Multiplier), typeof(double), typeof(NumberBox), new PropertyMetadata(1.0));
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(string), typeof(NumberBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        static NumberBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumberBox), new FrameworkPropertyMetadata(typeof(NumberBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (GetTemplateChild("PART_textBlock") is TextBlock textBlock)
+            {
+                textBlock.MouseLeftButtonDown += OnTextBlock_Mouse_LBD;
+                textBlock.MouseLeftButtonUp += OnTextBlock_Mouse_LBU;
+                textBlock.MouseMove += OnTextBlock_Mouse_Move;
+            }
+        }
+
+        private void OnTextBlock_Mouse_LBD(object sender, MouseButtonEventArgs e)
+        {
+            double.TryParse(Value, out _orginalValue);
+
+            Mouse.Capture(sender as UIElement);
+            _captured = true;
+            _valuChanged = false;
+            e.Handled = true;
+            _mouseXStart = e.GetPosition(this).X;
+        }
+
+        private void OnTextBlock_Mouse_LBU(object sender, MouseButtonEventArgs e)
+        {
+            if (_captured)
+            {
+                Mouse.Capture(null);
+                _captured = false;
+                e.Handled = true;
+                if (!_valuChanged && GetTemplateChild("PART_textBox") is TextBox textBox)
+                {
+                    textBox.Visibility = Visibility.Visible;
+                    textBox.Focus();
+                    textBox.SelectAll();
+                }
+            }
+        }
+
+        private void OnTextBlock_Mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (_captured)
+            {
+                var mouseX = e.GetPosition(this).X;
+                var d = mouseX - _mouseXStart;
+                if (Math.Abs(d) > SystemParameters.MinimumHorizontalDragDistance)
+                {
+                    if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control)) _multiplier = 0.001;
+                    else if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift)) _multiplier = 0.1;
+                    else _multiplier = 0.01;
+
+                    var newValue = _orginalValue + (d * _multiplier * Multiplier);
+                    Value = newValue.ToString("0.#####");
+                    _valuChanged = true;
+                }
+            }
+        }
+    }
+}
