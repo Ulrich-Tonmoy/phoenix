380591a35ff66a0b2cbc375273a618480101befd
Ulrich <tonmoyulrich@gmail.com>
2023-06-21 21:23:39 +0600

    feat: new script window

---
+++ src/Phoenix.Editor/GameDev/NewScriptDialog.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Phoenix.Editor.GameProject;
+using Phoenix.Editor.Utilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Phoenix.Editor.GameDev
+{
+    public partial class NewScriptDialog : Window
+    {
+        private static readonly string _cppCode = @"#include ""{0}.h""
+
+                                                    namespace {1}
+                                                    {{
+	                                                    REGISTER_SCRIPT({0});
+
+                                                        void {0}::begin_play()
+	                                                    {{
+
+	                                                    }}
+
+	                                                    void {0}::update(float dt)
+	                                                    {{
+
+	                                                    }}
+                                                    }}";
+        private static readonly string _hCode = @"#pragma once
+
+                                                    namespace {1}
+                                                    {{
+	                                                    class {0} :public phoenix::script::entity_script
+	                                                    {{
+	                                                    public:
+		                                                    constexpr explicit {0}(phoenix::game_entity::entity entity) :phoenix::script::entity_script{{entity}} {{}}
+                                                            void begin_play() override;
+		                                                    void update(float dt) override;
+                                                        private:
+	                                                    }};
+                                                    }}";
+        public NewScriptDialog()
+        {
+            InitializeComponent();
+        }
+
+        bool Validate()
+        {
+            bool isValid = false;
+            var name = scriptName.Text.Trim();
+            var path = scriptPath.Text.Trim();
+            string errorMsg = string.Empty;
+
+            if (string.IsNullOrEmpty(name))
+                errorMsg = "Type in a script name.";
+            else if (name.IndexOfAny(Path.GetInvalidFileNameChars()) != -1 || name.Any(x => char.IsWhiteSpace(x)))
+                errorMsg = "Invalid character(s) used in script name.";
+            if (string.IsNullOrEmpty(path))
+                errorMsg = "Select a valid script folder.";
+            else if (path.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+                errorMsg = "Invalid character(s) used in path.";
+            else if (!Path.GetFullPath(Path.Combine(Project.Current.Path, path)).Contains(Path.Combine(Project.Current.Path, @"Scripts\")))
+                errorMsg = "Script must be added to (a sub folder) Scripts.";
+            else if (File.Exists(Path.GetFullPath(Path.Combine(Path.Combine(Project.Current.Path, path), $"{name}.cpp"))) || File.Exists(Path.GetFullPath(Path.Combine(Path.Combine(Project.Current.Path, path), $"{name}.h"))))
+                errorMsg = $"Script {name} already exists in this folder.";
+            else
+                isValid = true;
+            if (!isValid)
+                messageTextBlock.Foreground = FindResource("Editor.RedBrush") as Brush;
+            else
+                messageTextBlock.Foreground = FindResource("Editor.FontBrush") as Brush;
+
+            messageTextBlock.Text = errorMsg;
+            return isValid;
+        }
+
+        private void OnScriptName_TexBox_TextChange(object sender, TextChangedEventArgs e)
+        {
+            if (!Validate()) return;
+            var name = scriptName.Text.Trim();
+            messageTextBlock.Text = $"{name}.h and {name}.cpp will be added to {Project.Current.Name}";
+        }
+
+        private void OnScriptPath_TexBox_TextChange(object sender, TextChangedEventArgs e)
+        {
+            Validate();
+        }
+
+        private async void OnCreate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validate()) return;
+            IsEnabled = false;
+
+            try
+            {
+                await Task.Run(() => CreateScript(name, path, solution, projectName));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Logger.Log(MessageType.Error, $"Failed to create script {scriptName.Text}");
+            }
+        }
+
+        private void CreateScript(string name, string path, string solution, string projectName)
+        {
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+            var cpp = Path.GetFullPath(Path.Combine(path, $"{name}.cpp"));
+            var h = Path.GetFullPath(Path.Combine(path, $"{name}.h"));
+
+            using (var sw = File.CreateText(cpp))
+            {
+
+            }
+            using (var sw = File.CreateText(h))
+            {
+
+            }
+        }
+    }
+}
---
+++ src/Phoenix.Editor/GameDev/VisualStudio.cs
@@ -0,0 +1,86 @@
+﻿using Phoenix.Editor.Utilities;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Phoenix.Editor.GameDev
+{
+    static class VisualStudio
+    {
+        private static EnvDTE80.DTE2 _vsInstance = null;
+        private static readonly string _progID = "VisualStudio.DTE.16.0";
+
+        [DllImport("ole32.dll")]
+        private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+        [DllImport("ole32.dll")]
+        private static extern int GetRunningObjectTable(uint reserved, out IRunningObjectTable pprot);
+        public static void OpenVisualStudio(string solutionPath)
+        {
+            IRunningObjectTable rot = null;
+            IEnumMoniker monikerTable = null;
+            IBindCtx bindCtx = null;
+            try
+            {
+                if (_vsInstance == null)
+                {
+                    var hResult = GetRunningObjectTable(0, out rot);
+                    if (hResult < 0 || rot == null) throw new COMException($"GetRunningObjectTable() returned HRESULT: {hResult:x8}");
+
+                    rot.EnumRunning(out monikerTable);
+                    monikerTable.Reset();
+
+                    hResult = CreateBindCtx(0, out bindCtx);
+                    if (hResult < 0 || bindCtx == null) throw new COMException($"CreateBindCtx() returned HRESULT: {hResult:x8}");
+
+                    IMoniker[] currentMoniker = new IMoniker[1];
+                    while (monikerTable.Next(1, currentMoniker, IntPtr.Zero) == 0)
+                    {
+                        string name = string.Empty;
+                        currentMoniker[0]?.GetDisplayName(bindCtx, null, out name);
+                        if (name.Contains(_progID))
+                        {
+                            hResult = rot.GetObject(currentMoniker[0], out object obj);
+                            if (hResult < 0 || obj == null) throw new COMException($"GetObject() returned HRESULT: {hResult:x8}");
+
+                            EnvDTE80.DTE2 dte = obj as EnvDTE80.DTE2;
+                            var solutionName = dte.Solution.FullName;
+                            if (solutionName == solutionPath)
+                            {
+                                _vsInstance = dte;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (_vsInstance == null)
+                    {
+                        Type visualStudioType = Type.GetTypeFromProgID(_progID, true);
+                        _vsInstance = Activator.CreateInstance(visualStudioType) as EnvDTE80.DTE2;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Logger.Log(MessageType.Error, "Failed to open Visual Studio");
+            }
+            finally
+            {
+                if (monikerTable != null) Marshal.ReleaseComObject(monikerTable);
+                if (rot != null) Marshal.ReleaseComObject(rot);
+                if (bindCtx != null) Marshal.ReleaseComObject(bindCtx);
+            }
+        }
+
+        public static void CloseVisualStudio()
+        {
+            if (_vsInstance?.Solution.IsOpen == true)
+            {
+                _vsInstance.ExecuteCommand("File.SaveAll");
+                _vsInstance.Solution.Close(true);
+            }
+            _vsInstance?.Quit();
+        }
+    }
+}
