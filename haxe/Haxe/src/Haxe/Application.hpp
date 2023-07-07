abd7ba5f99ac45a4dbf692c4e656aebb9bf83c72
Ulrich <tonmoyulrich@gmail.com>
2023-07-07 18:11:08 +0600

    chore: dispatch event from gflw

--- haxe/Haxe/src/Haxe/Application.hpp
+++ haxe/Haxe/src/Haxe/Application.hpp
@@ -2,6 +2,7 @@
 
 #include "Core.hpp"
 #include "Events/Event.hpp"
+#include "Haxe/Events/ApplicationEvent.hpp"
 #include "Window.hpp"
 
 namespace Haxe
@@ -13,7 +14,9 @@ namespace Haxe
 		virtual ~Application();
 
 		void Run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
