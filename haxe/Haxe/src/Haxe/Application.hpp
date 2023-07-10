ae4fd5e02b427e95d5a839c58fcebff3f7209f1f
Ulrich <tonmoyulrich@gmail.com>
2023-07-10 18:43:15 +0600

    chore: Added vertex array support

--- haxe/Haxe/src/Haxe/Application.hpp
+++ haxe/Haxe/src/Haxe/Application.hpp
@@ -11,6 +11,7 @@
 
 #include "Haxe/Renderer/Shader.hpp"
 #include "Haxe/Renderer/Buffer.hpp"
+#include "Haxe/Renderer/VertexArray.hpp"
 
 namespace Haxe
 {
@@ -36,10 +37,11 @@ namespace Haxe
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	private:
 		static Application* s_Instance;
 	};
