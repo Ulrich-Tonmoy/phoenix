bcbfea680e6bdc71a5f6334cd526d6e74b8d9a27
Ulrich <tonmoyulrich@gmail.com>
2023-07-10 18:34:51 +0600

    chore: Added buffer layout descriptors for vertex buffers.

--- haxe/Haxe/src/Haxe/Application.cpp
+++ haxe/Haxe/src/Haxe/Application.cpp
@@ -13,6 +13,27 @@ namespace Haxe
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Haxe::ShaderDataType::Float:    return GL_FLOAT;
+		case Haxe::ShaderDataType::Float2:   return GL_FLOAT;
+		case Haxe::ShaderDataType::Float3:   return GL_FLOAT;
+		case Haxe::ShaderDataType::Float4:   return GL_FLOAT;
+		case Haxe::ShaderDataType::Mat3:     return GL_FLOAT;
+		case Haxe::ShaderDataType::Mat4:     return GL_FLOAT;
+		case Haxe::ShaderDataType::Int:      return GL_INT;
+		case Haxe::ShaderDataType::Int2:     return GL_INT;
+		case Haxe::ShaderDataType::Int3:     return GL_INT;
+		case Haxe::ShaderDataType::Int4:     return GL_INT;
+		case Haxe::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		HX_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		HX_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -27,16 +48,36 @@ namespace Haxe
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -45,10 +86,15 @@ namespace Haxe
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -58,9 +104,12 @@ namespace Haxe
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			in vec4 v_Color;
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
