aeee5deca7d17faa7364f71d6a81edc71f2d0d99
Ulrich <tonmoyulrich@gmail.com>
2023-07-10 15:50:32 +0600

    chore: input polling

--- haxe/Haxe/src/Haxe/Window.hpp
+++ haxe/Haxe/src/Haxe/Window.hpp
@@ -37,6 +37,8 @@ namespace Haxe
 		virtual void SetVSync(bool enabled) = 0;
 		virtual bool IsVSync() const = 0;
 
+		virtual void* GetNativeWindow() const = 0;
+
 		static Window* Create(const WindowProps& props = WindowProps());
 	};
 
