22f1d679e701d3ade78f59e5aac0292b40c106ed
Ulrich <ulrichtonmoy1@gmail.com>
2023-07-24 17:52:39 +0600

    chore: setting up geometry pipeline

---
+++ src/AssetTools/Geometry.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "ToolsCommon.hpp"
+
+namespace phoenix::tools
+{
+	struct mesh
+	{
+		utl::vector<math::v3> positions;
+		utl::vector<math::v3> normals;
+		utl::vector<math::v3> tangents;
+		utl::vector<utl::vector<math::v2>> uv_sets;
+
+		utl::vector<u32> raw_indices;
+
+
+	};
+
+	struct lod_group
+	{
+		std::string name;
+		utl::vector<mesh> meshes;
+	};
+
+	struct scene
+	{
+		std::string name;
+		utl::vector<lod_group> lod_groups;
+	};
+
+	struct geometry_import_settings
+	{
+		f32 smoothing_angle;
+		u8 calculate_normals;
+		u8 calculate_tangents;
+		u8 reverse_handedness;
+		u8 import_embeded_textures;
+		u8 import_animations;
+	};
+
+	struct scene_data
+	{
+		u8* buffer;
+		u32 buffer_size;
+		geometry_import_settings settings;
+	};
+}
---
+++ src/AssetTools/ToolsCommon.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "CommonHeaders.hpp"
+
+#ifndef EDITOR_INTERFACE
+#define EDITOR_INTERFACE extern "C" __declspec(dllexport)
+#endif
