19d337f230fa4bfba7822da5bb8b0e9886e6c5c4
Ulrich <ulrichtonmoy1@gmail.com>
2023-09-29 19:42:12 +0600

    refactor: moved shader and mesh -> engine.zig

---
+++ src-zig/src/engine.zig
@@ -0,0 +1,83 @@
+const gl = @import("gl");
+
+pub const Mesh = struct {
+    vertices: [32]f32 = [1]f32{0} ** 32,
+    indices: [32]u32 = [1]u32{0} ** 32,
+
+    vertexCount: isize = 0,
+    indexCount: isize = 0,
+
+    vao: u32 = undefined,
+    vbo: u32 = undefined,
+    ibo: u32 = undefined,
+
+    const Self = @This();
+
+    pub fn create(self: *Self) void {
+        // VAO VBO IBO
+        gl.genVertexArrays(1, &self.vao);
+        gl.genBuffers(1, &self.vbo);
+        gl.genBuffers(1, &self.ibo);
+
+        gl.bindVertexArray(self.vao);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, self.vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, self.vertexCount * @sizeOf(f32), self.vertices[0..].ptr, gl.STATIC_DRAW);
+
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), null);
+        gl.enableVertexAttribArray(0);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ibo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, self.indexCount * @sizeOf(u32), self.indices[0..].ptr, gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, 0);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0);
+        gl.bindVertexArray(0);
+    }
+
+    pub fn bind(self: Self) void {
+        gl.bindVertexArray(self.vao);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, self.ibo);
+        gl.drawElements(gl.TRIANGLES, @intCast(self.indexCount), gl.UNSIGNED_INT, null);
+    }
+
+    pub fn deinit(self: Self) void {
+        gl.deleteVertexArrays(1, &self.vao);
+        gl.deleteBuffers(1, &self.vbo);
+        gl.deleteBuffers(1, &self.ibo);
+    }
+};
+
+pub const Shader = struct {
+    program: u32 = 0,
+    vertSource: []const u8,
+    fragSource: []const u8,
+
+    const Self = @This();
+
+    pub fn compile(self: *Self) void {
+        var vertShader: u32 = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertShader, 1, &self.vertSource.ptr, null);
+        gl.compileShader(vertShader);
+
+        var fragShader: u32 = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragShader, 1, &self.fragSource.ptr, null);
+        gl.compileShader(fragShader);
+
+        self.program = gl.createProgram();
+        gl.attachShader(self.program, vertShader);
+        gl.attachShader(self.program, fragShader);
+        gl.linkProgram(self.program);
+
+        gl.deleteShader(vertShader);
+        gl.deleteShader(fragShader);
+    }
+
+    pub fn bind(self: Self) void {
+        gl.useProgram(self.program);
+    }
+
+    pub fn deinit(self: Self) void {
+        gl.deleteProgram(self.program);
+    }
+};
