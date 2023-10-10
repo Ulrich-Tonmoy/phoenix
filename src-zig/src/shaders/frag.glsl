ce595ae25a671ddd47fd0f63dd97dcb037e9a06f
Ulrich <ulrichtonmoy1@gmail.com>
2023-10-10 17:32:44 +0600

    chore: chartreuse color sphere

--- src-zig/src/shaders/frag.glsl
+++ src-zig/src/shaders/frag.glsl
@@ -3,6 +3,8 @@ out vec4 FragColor;
 
 in vec3 out_norm;
 
+uniform vec4 _Color;
+
 void main() {
   vec3 n = out_norm;
   vec3 l = normalize(vec3(0.5, 0.5, -0.5));
@@ -11,5 +13,5 @@ void main() {
   vec3 diffuse = vec3(1.0, 1.0, 1.0);
   vec3 color = diffuse * light * 2;
 
-  FragColor = vec4(color, 1.0);
+  FragColor = vec4(color, 1.0) * _Color;
 }
